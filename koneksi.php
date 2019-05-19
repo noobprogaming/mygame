a2484e95d7ca2b687aa495371150cdf024e8085e
alvinbintang <alvinrr91@gmail.com>
2019-05-19 21:08:09 +0700

    Fix

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,9 @@
 <?php 
-<<<<<<< HEAD
-/*$koneksi = mysqli_connect("localhost","u614737895_pk","dev_varokah","u614737895_pk");*/
-$koneksi = mysqli_connect("localhost","root","","my_game");
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
-=======
-$koneksi = mysqli_connect("localhost","u614737895_pk","dev_varokah","u614737895_pk");
+
+$koneksi = mysqli_connect("localhost","root","","mygame");
  
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
  
 ?>
->>>>>>> d53a0245293947bdc443834dcbe164168ecc76f1
