2f1fb74c28c60304d266de5f4ee31f3509806ad8
lucasarbi <lucasarby00@gmail.com>
2019-07-15 16:54:48 +0700

    gambar

---
+++ profile/finalfantasy7.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id"
+        content="562498474090-rmhpeunoatnlg7lv8b9buije5n0n2r9t.apps.googleusercontent.com">
+
+    <title>Punyakita</title>
+    <link rel="icon" type="image/png" href="../assets/img/lg.png" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="../assets/custom.css">
+    <style>
+
+    </style>
+</head>
+
+<body onload="gender()">
+    <?php 
+session_start();
+if($_SESSION['status']!="login"){
+	header("location:../index.php?msg=warning");
+}
+
+include '../koneksi.php';
+$email=$_SESSION['email'];
+include '../sql.php';
+?>
+
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top px-5">
+            <img src="../assets/img/lg.png" class="logo logo-small">
+            <a class="navbar-brand text-shadow mx-3" href="index.php">Punyakita</a>
+            <button class="btn-menu" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="compare.php">Compare</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item active dropdown">
+                        <a class="nav-link">PC</a>
+                        <div class="dropdown-menu dropdown-content black-soft box-shadow"
+                            aria-labelledby="navbarDropdown">
+                            <div class="px-3 py-1" style="color: rgb(255, 255, 255);">
+                                <ul style="list-style: none; width: 170px;">
+                                    <a href="news.php" style="text-decoration: none;">
+                                        <li class="dd my-3">News</li>
+                                    </a>
+                                    <a href="genre.php" style="text-decoration: none;">
+                                        <li class="dd my-3">Game</li>
+                                    </a>
+                                    <a href="peripheral.php" style="text-decoration: none;">
+                                        <li class="dd my-3">Periferal</li>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="search-box">
+                            <input type="text" autocomplete="off" placeholder="Cari Game . . " />
+                            <div class="result"></div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="profile.php"><i class="fa fa-user"> </i> <?php echo $u_id; ?></a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-content black-soft box-shadow"
+                            aria-labelledby="navbarDropdown">
+                            <form action="logout.php" method="post">
+                                <div class="px-3 py-1" style="width: 15rem">
+                                    <table>
+                                        <tr>
+                                            <td>
+                                                <a href="profile.php">
+                                                    <img src="<?php echo $u_pp; ?>" alt="Tidak ada foto"
+                                                        class="img-profile">
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="profile.php" style="text-decoration: none;">
+                                                    <p class="fg-white"><?php echo $u_name; ?></p>
+                                                </a>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                <input type="submit" value="Logout" onclick="signOut();"
+                                                    class="btn btn-sm input-btn w100">
+                                                <button onclick="signOut();">Out
+                                                </button>
+                                                <button onclick="myFunction();">Out
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="row">
+        <div class="mx-5 my-5">
+            <p class="gg" style="font-size: 30px;">
+                GAME GENRE
+            </p>
+            <div class="col-lg-12 mx-auto my-5">
+                <div class="row">
+                    <div class="col-lg-8 px-0 py-0">
+                        <img class="img-fluid" src="../assets/img/1.jpg">
+                    </div>
+                    <div class="col-lg-4 px-0 py-0">
+                        <img class="img-fluid" src="../assets/img/2.jpg">
+                        <img class="img-fluid" src="../assets/img/3.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid px-5 ">
+
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="row">
+                        <div class="col-md-12">
+                                <h2>Square Enix Bantah Rilis FF VII Remake untuk Xbox One</h2>
+                                <p class="lead"><img width="500" height="500" src="../assets/img/photo/fin.jpg"
+                                        class="img-fluid" /><br>
+                                        Apakah Final Fantasy VII Remake akan menjadi game multi-platform? Indikasi 
+                                        yang muncul memang mengarah demikian. Ketika trailer perdananya diperkenalkan 
+                                        kepada publik via panggung utama E3 beberapa tahun yang lalu, ia ditutup 
+                                        dengan kelimat “Play it first on Playstation 4” yang memang jadi jargon dan 
+                                        konfirmasi tidak langsung, bahwa pada akhirnya Final Fantasy VII Remake akan 
+                                        dirilis di platform lain. Tetapi ada sesuatu yang berubah di trailer-trailer 
+                                        teranyarnya. Tulisan tersebut tidak lagi mengemuka dan hanya digantikan 
+                                        “Playstation 4” saja membuat pertanyaan soal rilis multi-platformnya kembali 
+                                        menjadi misteri. Hingga halaman Xbox tak sengaja melepas iklan di akun Facebook 
+                                        resmi lengkap dengan tanggal rilis yang sama.
+                                    <img width="500" height="500" src="../assets/img/photo/fina.jpg"
+                                        class="img-fluid" />><br>
+                                        Iklan tersebut langsung ditangkap gamer dan memicu diskusi, walaupun pada 
+                                        akhirnya dibantah sendiri oleh tim Xbox yang menyebutnya tidak lebih dari 
+                                        sekedar kekeliruan internal semata dengan post yang berakhir dihapus. 
+                                        Tetapi tidak sedikit gamer yang yakin bahwa alih-alih “kekeliruan”, ini 
+                                        adalah sebuah pengumuman resmi yang tidak sengaja bocor lebih awal, bahwa 
+                                        FF VII Remake akan dirilis di Xbox One bersama dengan versi PS4 di bulan Maret 2020 mendatang.
+                                    <img width="500" height="500" src="../assets/img/photo/final.jpg"
+                                        class="img-fluid" />><br>
+                                        Untungnya, Square Enix akhirnya angkat bicara. Dalam pernyataan resmi yang 
+                                        mereka bagi ke beberapa media gaming besar, Square Enix secara tegas menyebut 
+                                        bahwa Final Fantasy VII Remake hanya akan tersedia di Playstation 4 pada hari 
+                                        rilisnya nanti – 3 Maret 2020 mendatang. Untuk saat ini, mereka tidak memiliki 
+                                        rencana apapun untuk merilisnya di platform yang lain. Walaupun tidak lantas 
+                                        “mengubur” potensi rilis multi-platform, namun informasi ini secara otomatis 
+                                        mematikan harapan bahwa FF VII Remake akan tersedia di luar PS4 pada saat hari 
+                                        rilis nanti.
+
+                                        Bagaimana dengan Anda sendiri? Berapa banyak dari Anda yang akan tertarik 
+                                        melirik PS4 hanya untuk memainkan proyek yang begitu diantisipasi ini?
+
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Artikel</h4>
+                        <hr>
+                        <p class="artikel">Spesifikasi PC untuk Man of Medan</p>
+                        <hr>
+                        <p class="artikel">Spesifikasi PC untuk Code Vein</p>
+                        <hr>
+                        <p class="artikel">Kingdom Under Fire II Tetap Hidup,
+                            Versi PC Tuju Barat Tahun Ini</p>
+                        <hr>
+                        <p class="artikel">Menjajal DEMO Pamali: Horror Indonesia Potensial!</p>
+                        <hr>
+                        <p class="artikel">Microsoft akan Hadirkan Login Windows 10 Tanpa Password</p>
+                        <hr>
+                        <p class="artikel">Review Cuphead: Kartun untuk Kaum Masokis!</p>
+                        <hr>
+                        <p class="artikel">Review Frostpunk: Semua Demi Sebuah Kehangatan!</p>
+                        <hr>
+                        <p class="artikel">Review Doki Doki Literature Club: Jangan Baca, Main Saja Dulu!L</p>
+                        <hr>
+                        <p class="artikel">Review Outlast 2: Tidak Lebih Baik!</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="page-footer text-center black-soft">
+        <div class="row" style="margin-right: 0!important;">
+            <div class="col-md-4 p5rem" style="height: 300px;">
+                <div class="row">
+                    <img src="../assets/img/lg.png" class="logo logo-big">
+                </div>
+                <div class="row my-3">
+                    <div class="sosmed mx-1 my-1">
+                        <i class="fa fa-facebook fa-lg"></i>
+                    </div>
+                    <div class="sosmed mx-1 my-1">
+                        <i class="fa fa-twitter fa-lg"></i>
+                    </div>
+                    <div class="sosmed mx-1 my-1">
+                        <i class="fa fa-instagram fa-lg"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 p5rem text-white">
+                <ul class="fo-ul">
+                    <li class="text-white">Artikel</li>
+                    <li>
+                        <a href>
+                            NEWS
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            GAME
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            PERIPHERAL
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 p5rem text-white">
+                <ul class="fo-ul">
+                    <li class="text-white">Artikel</li>
+                    <li>
+                        <a href>
+                            NEWS
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            GAME
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            PERIPHERAL
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="red" style="height: 1px;">
+        </div>
+        <div class="pb-4 black text-white" style="height: 50px; padding-top: 10px;">
+            <p> © 2019 Tech </p>
+        </div>
+    </footer>
+
+</body>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="../assets/custom.js"></script>
+<script src="../assets/search.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+</html>
