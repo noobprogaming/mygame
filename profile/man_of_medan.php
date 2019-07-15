7ce56fde95e0171c5fa6d0da27f19fe4e4c3bbd1
agilsjech <agilbsa10@gmail.com>
2019-07-15 21:45:09 +0700

    kerja

---
+++ profile/man_of_medan.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id"
+        content="562498474090-rmhpeunoatnlg7lv8b9buije5n0n2r9t.apps.googleusercontent.com">
+
+    <title>Punyakita</title>
+    <link rel="icon" type="image/png" href="../assets/img/lg.png" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="../assets/custom.css">
+    <style>
+
+    </style>
+</head>
+
+<body onload="gender()">
+    <?php 
+session_start();
+if($_SESSION['status']!="login"){
+	header("location:../index.php?msg=warning");
+}
+
+include '../koneksi.php';
+$email=$_SESSION['email'];
+include '../sql.php';
+?>
+
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top px-5">
+            <img src="../assets/img/lg.png" class="logo logo-small">
+            <a class="navbar-brand text-shadow mx-3" href="index.php">Punyakita</a>
+            <button class="btn-menu" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="compare.php">Compare</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item active dropdown">
+                        <a class="nav-link">PC</a>
+                        <div class="dropdown-menu dropdown-content black-soft box-shadow"
+                            aria-labelledby="navbarDropdown">
+                            <div class="px-3 py-1" style="color: rgb(255, 255, 255);">
+                                <ul style="list-style: none; width: 170px;">
+                                    <a href="news.php" style="text-decoration: none;">
+                                        <li class="dd my-3">News</li>
+                                    </a>
+                                    <a href="genre.php" style="text-decoration: none;">
+                                        <li class="dd my-3">Game</li>
+                                    </a>
+                                    <a href="peripheral.php" style="text-decoration: none;">
+                                        <li class="dd my-3">Periferal</li>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="search-box">
+                            <input type="text" autocomplete="off" placeholder="Cari Game . . " />
+                            <div class="result"></div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="profile.php"><i class="fa fa-user"> </i> <?php echo $u_id; ?></a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-content black-soft box-shadow"
+                            aria-labelledby="navbarDropdown">
+                            <form action="logout.php" method="post">
+                                <div class="px-3 py-1" style="width: 15rem">
+                                    <table>
+                                        <tr>
+                                            <td>
+                                                <a href="profile.php">
+                                                    <img src="<?php echo $u_pp; ?>" alt="Tidak ada foto"
+                                                        class="img-profile">
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="profile.php" style="text-decoration: none;">
+                                                    <p class="fg-white"><?php echo $u_name; ?></p>
+                                                </a>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                <input type="submit" value="Logout" onclick="signOut();"
+                                                    class="btn btn-sm input-btn w100">
+                                                <button onclick="signOut();">Out
+                                                </button>
+                                                <button onclick="myFunction();">Out
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="row">
+        <div class="mx-5 my-5">
+            <p class="gg" style="font-size: 30px;">
+                MAN OF MEDAN
+            </p>
+            <div class="col-lg-12 mx-auto my-5">
+                <div class="row">
+                    <div class="col-lg-8 px-0 py-0">
+                    <img class="img-fluid" src="../assets/img/photo/mom5.jpg">
+                    </div>
+                    <div class="col-lg-4 px-0 py-0">
+                        <img class="img-fluid" src="../assets/img/photo/mom3.jpg">
+                        <img class="img-fluid" src="../assets/img/photo/mom4.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid px-5 ">
+
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="row">
+                        <div class="col-md-12">
+                                <h2>Square Enix Bantah Rilis FF VII Remake untuk Xbox One</h2>
+                                <p class="lead"><img width="500" height="500" src="../assets/img/photo/mom1.jpg"
+                                        class="img-fluid" /><br>
+                                        Kejutan yang pantas untuk disambut dengan tangan terbuka, 
+                                        ini mungkin reaksi sebagian besar gamer dengan langkah yang dilakukan oleh Supermassive Games dengan The Dark Pictures Anthology. 
+                                        Setelah punya citra yang kuat sebagai developer “eksklusif” Sony lewat game-game mereka di masa lalu, 
+                                        Supermassive Games akhirnya bergerak menuju multi-platform dengan proyek terbaru yang akan dimulai dengan Man of Medan ini. 
+                                        Bagi gamer PC, ini tentu jadi ekstra pekerjaan baru untuk mulai mempersiapkan diri, 
+                                        apalagi dengan detail visual game Supermassive Games yang terhitung memesona. Berita baiknya? 
+                                        Informasi tersebut akhirnya mengemuka.<br>
+                                    <img width="500" height="500" src="../assets/img/photo/mom2.jpg"
+                                        class="img-fluid" /><br>
+                                        Mendekati waktu rilis yang rencananya akan meluncur akhir bulan depan, 
+                                        Bandai Namco dan Supermassive Games akhirnya membuka halaman Store Man of Medan untuk Steam. 
+                                        Proses pre-order bahkan sudah dibuka dengan harga “hanya” 219 ribu Rupiah saja, 
+                                        walaupun tanpa detail informasi berapa panjang sisi cerita yang akan ia usung. 
+                                        Untuk Anda yang tidak familiar, Man of Medan akan memuat cerita soal sekelompok anak muda yang terjebak di sebuah kapal hantu. 
+                                        Mekanik khas Supermassive – dimana opsi yang Anda pilih akan menentukan siapa yang hidup dan mati juga akan dipertahankan di sini<br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Artikel</h4>
+                        <hr>
+                        <p class="artikel">Spesifikasi PC untuk Man of Medan</p>
+                        <hr>
+                        <p class="artikel">Spesifikasi PC untuk Code Vein</p>
+                        <hr>
+                        <p class="artikel">Kingdom Under Fire II Tetap Hidup,
+                            Versi PC Tuju Barat Tahun Ini</p>
+                        <hr>
+                        <p class="artikel">Menjajal DEMO Pamali: Horror Indonesia Potensial!</p>
+                        <hr>
+                        <p class="artikel">Microsoft akan Hadirkan Login Windows 10 Tanpa Password</p>
+                        <hr>
+                        <p class="artikel">Review Cuphead: Kartun untuk Kaum Masokis!</p>
+                        <hr>
+                        <p class="artikel">Review Frostpunk: Semua Demi Sebuah Kehangatan!</p>
+                        <hr>
+                        <p class="artikel">Review Doki Doki Literature Club: Jangan Baca, Main Saja Dulu!L</p>
+                        <hr>
+                        <p class="artikel">Review Outlast 2: Tidak Lebih Baik!</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="page-footer text-center black-soft">
+        <div class="row" style="margin-right: 0!important;">
+            <div class="col-md-4 p5rem" style="height: 300px;">
+                <div class="row">
+                    <img src="../assets/img/lg.png" class="logo logo-big">
+                </div>
+                <div class="row my-3">
+                    <div class="sosmed mx-1 my-1">
+                        <i class="fa fa-facebook fa-lg"></i>
+                    </div>
+                    <div class="sosmed mx-1 my-1">
+                        <i class="fa fa-twitter fa-lg"></i>
+                    </div>
+                    <div class="sosmed mx-1 my-1">
+                        <i class="fa fa-instagram fa-lg"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 p5rem text-white">
+                <ul class="fo-ul">
+                    <li class="text-white">Artikel</li>
+                    <li>
+                        <a href>
+                            NEWS
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            GAME
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            PERIPHERAL
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 p5rem text-white">
+                <ul class="fo-ul">
+                    <li class="text-white">Artikel</li>
+                    <li>
+                        <a href>
+                            NEWS
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            GAME
+                        </a>
+                    </li>
+                    <li>
+                        <a href>
+                            PERIPHERAL
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="red" style="height: 1px;">
+        </div>
+        <div class="pb-4 black text-white" style="height: 50px; padding-top: 10px;">
+            <p> © 2019 Tech </p>
+        </div>
+    </footer>
+
+</body>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="../assets/custom.js"></script>
+<script src="../assets/search.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+</html>
